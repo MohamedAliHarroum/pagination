28c3025a4d45347eb38cd543cb4de8204d29efc4
MohamedAliHarroum <mohamedali.harroum@gmail.com>
2022-07-09 16:50:11 +0100

    first commit

---
+++ src/main/java/com/example/pagination/service/implementation/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.pagination.service.implementation;
+
+import com.example.pagination.domain.User;
+import com.example.pagination.repository.UserRepository;
+import com.example.pagination.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl  implements UserService {
+    private  final UserRepository userRepository;
+
+    @Override
+    public Page<User> gerUsers(String name, int page, int size) {
+        log.info("Fetching users from page {} of size {}",page,size);
+        return userRepository.findByNameContaining(name, PageRequest.of(page,size));
+    }
+}
